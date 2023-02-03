8b1d4036a483b55dfef5c1332efc4139fea1b806
Topher McCulloch <topher@pivotdesign.com>
2023-02-03 13:25:35 -0600

    Updates social icons

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'type_child_enqueue_styles' );
 function type_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), '1.0.5' );
 }
 
 add_post_type_support( 'page', 'excerpt' );
